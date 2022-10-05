37e8b0e3c11ce63d919a35663e87a01e5da6c535
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-10-05 18:03:52 +0300

    move logic from template to class[MT-28]

--- src/app/shared/form-controls/form-input/form-input.component.ts
+++ src/app/shared/form-controls/form-input/form-input.component.ts
@@ -1,21 +1,66 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  Validators,
+} from '@angular/forms';
+
+enum InputType {
+  password = 'password',
+  text = 'text',
+  email = 'email',
+}
 
 @Component({
-  selector: 'app-form-input',
+  selector: 'tx-form-input',
   templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.scss']
+  styleUrls: ['./form-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormInputComponent),
+      multi: true,
+    },
+  ],
 })
-export class FormInputComponent implements OnInit {
-  @Input() InputForm!: FormGroup;
+export class FormInputComponent implements ControlValueAccessor {
+  formControl: FormControl = new FormControl({}, [Validators.required]);
   @Input() type: string = '';
   @Input() placeholder: string = '';
-  
-  hide: boolean = false;
 
-  constructor() { }
+  isPasswordVisible: boolean = false;
+  isDisabled: boolean = false;
 
-  ngOnInit(): void {
+  onTouched = (): void => {};
+
+  writeValue(obj: string): void {
+    this.formControl.setValue(obj);
+    if (this.type === InputType.email) {
+      this.formControl.addValidators(Validators.email);
+    }
+  }
+  registerOnChange(fn: any): void {
+    this.formControl.valueChanges.subscribe(fn);
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 
+  getInputType(): InputType {
+    return !this.isPasswordVisible && this.type === InputType.password
+      ? InputType.password
+      : InputType.text;
+  }
+
+  isPasswordType(): boolean {
+    return this.type === InputType.password;
+  }
+
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
 }
--- src/app/tx.module.ts
+++ src/app/tx.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { AppComponent } from './tx.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
