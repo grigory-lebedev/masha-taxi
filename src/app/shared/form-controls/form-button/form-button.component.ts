146341505f30239701f43f38d7cbf6f77180ecf2
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-10-07 18:19:54 +0300

    remove ValueAccessor from button component[MT-30]

--- src/app/shared/form-controls/form-button/form-button.component.ts
+++ src/app/shared/form-controls/form-button/form-button.component.ts
@@ -1,37 +1,11 @@
-import { Component, Input, Output } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tx-form-button',
   templateUrl: './form-button.component.html',
   styleUrls: ['./form-button.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: FormButtonComponent,
-      multi: true,
-    },
-  ],
 })
-export class FormButtonComponent implements ControlValueAccessor {
+export class FormButtonComponent {
   @Input() text: string = '';
   @Input() disabled: boolean = true;
-
-  formControl: FormControl = new FormControl({value: '', disabled: this.disabled});
-
-  onTouched!: () => void;
-  onChange: (value: boolean) => void = () => {};
-
-  writeValue(obj: boolean): void {
-    this.formControl.setValue(obj);
-  }
-  registerOnChange(fn: any): void {
-    this.formControl.valueChanges.subscribe(fn);
-  }
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-  setDisabledState?(isDisabled: boolean): void {
-   this.disabled = isDisabled;
-  }
 }
--- src/app/tx.component.ts
+++ src/app/tx.component.ts
@@ -7,28 +7,27 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./tx.component.scss'],
 })
 export class AppComponent {
-  title = 'masha-taxi';
-  selectItems = [
+  public title = 'masha-taxi';
+  public selectItems = [
     { id: 1, value: 'Client' },
     { id: 2, value: 'Driver' },
   ];
 
   public demonstrationForm: FormGroup = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl({value: '', disabled: false}, Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl({ value: '', disabled: false },Validators.required),
     name: new FormControl('', Validators.required),
     role: new FormControl('', Validators.required),
     isLoggedIn: new FormControl(false),
-    button: new FormControl({value: '', disabled: true}),
   });
 
-  isFormInvalid(): boolean{
-    return !this.demonstrationForm.valid;
+  public isFormValid(): boolean {
+    return this.demonstrationForm.valid;
   }
 
-  onSignIn(): void {
-    console.log((this.demonstrationForm.controls)['password']);
-    if (this.isFormInvalid()) {
+  public onSignIn(): void {
+    // console.log((this.demonstrationForm.controls)['password']);
+    if (!this.isFormValid()) {
       return;
     }
   }
