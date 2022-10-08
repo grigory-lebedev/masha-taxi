9c9e9adeb90e2849424914065c82eafcec9612e5
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-10-08 17:50:20 +0300

    fix styles [MT-27]

--- src/app/shared/general-components/page-wrapper/page-wrapper.component.ts
+++ src/app/shared/general-components/page-wrapper/page-wrapper.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
     styleUrls: ["./page-wrapper.component.scss"]
 })
 export class PageWrapperComponent {
-    
+    public isLoggedIn: boolean = false;
 }
