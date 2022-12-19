35efaac01154f9f0e635732078a6d64ad10defec
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-12-19 17:09:42 +0300

    Create directive module [MT-39]

---
+++ src/app/shared/modules/directives.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { NumericInputDirective } from 'src/app/modules/auth/sign-up/components/car-form/numeric-input.directive';
+
+@NgModule({
+  declarations: [NumericInputDirective]
+})
+export class DirectivesModule {}
